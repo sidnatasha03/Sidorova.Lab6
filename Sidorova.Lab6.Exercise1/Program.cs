d8eb4cdb64d5b65c0c0d5cd5a23a8021cb790368
sidnatasha03 <90253059+sidnatasha03@users.noreply.github.com>
2021-11-16 11:21:20 +0300

    Add files via upload

---
+++ Sidorova.Lab6.Exercise1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Sidorovaa.Lab6.Exercise1
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int c = Convert.ToInt32(Console.ReadLine());
+            int[] array = new int[c];
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = rand.Next(-100, 100);
+            } 
+            //создание массива
+
+            //цикл вывести
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine(array[i]);
+            }
+            Console.WriteLine("_____________________");
+            for (int i = 0; i < array.Length; i++) 
+            {
+                if (array[i] < 0)
+                    array[i] = 3;
+            }
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine(array[i]);
+            }
+
+        }
+    }
+}
